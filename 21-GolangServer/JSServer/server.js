58ad763de7c8704edc2eb92dd38ef9c90fa507eb
MonalBarse <monal.barse.cer22@itbhu.ac.in>
2024-07-01 21:00:52 +0530

    chore: added explanation

--- 21-GolangServer/JSServer/server.js
+++ 21-GolangServer/JSServer/server.js
@@ -1,10 +1,35 @@
 const express = require("express");
-const app = express();
-
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+const app = express();
 
+// Middleware functions
+const validateContentType = (contentType) => {
+  return (req, res, next) => {
+    if (req.headers["content-type"] !== contentType) {
+      return res.status(400).send({
+        error: "Invalid Content-Type",
+        message: `Expected ${contentType}`,
+      });
+    }
+    next();
+  };
+};
+
+const handleJSONParsingErrors = (err, req, res, next) => {
+  if (err instanceof SyntaxError) {
+    return res.status(400).send({
+      error: "Invalid JSON",
+      message: err.message,
+    });
+  }
+  next();
+};
+
+// Middleware to parse application/x-www-form-urlencoded data globally
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the GET route!");
 });
@@ -14,17 +39,24 @@ app.get("/get", (req, res) => {
   res.send(`Hello, ${name}! (GET request)`);
 });
 
-app.post("/", (req, res) => {
-  const name = req.body.name || "Unnamed visitor";
-  res.send(`Hello, ${name}! (POST request)`);
+app.post("/post", bodyParser.json(), (req, res) => {
+  let dataReceived = req.body;
+  res.status(200).send(dataReceived);
 });
-// Accept any JSON data and return it back to the client
-app.post("/post", (req, res) => {
-  const recievedData = req.body;
-  // We recieve raw JSON data and send it back as a response
 
-  res.json(recievedData);
-});
+app.post(
+  "/form",
+  validateContentType("application/x-www-form-urlencoded"),
+  bodyParser.urlencoded({ extended: true }),
+  (req, res) => {
+    let dataReceived = req.body;
+    res.status(200).send(JSON.stringify(dataReceived));
+  },
+);
+
+// Error handling middleware
+app.use(handleJSONParsingErrors);
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
--- 21-GolangServer/main.go
+++ 21-GolangServer/main.go
@@ -13,53 +13,78 @@ const URL = "http://localhost:3000"
 func main() {
 
 	fmt.Println("Server in Golang")
-	// PerformGetRequest(URL)
+	PerformGetRequest(URL)
 	PerformPostFormRequest(URL + "/post")
 
 }
 
-func PerformPostFormRequest(u string) {
-	fmt.Println("Performing POST request to " + u)
-	// Form data
-
-	data := url.Values{}
-	data.Add("name", "Monal")
-	data.Add("age", "22")
-	data.Add("job", "Web Developer")
-
-	res, err := http.PostForm(u, data)
-	checkError(err)
-
-	content, err := io.ReadAll(res.Body)
-	checkError(err)
-	fmt.Println("Response body:", string(content))
-
-}
+// ----------------- GET Request ----------------- //
 
 func PerformGetRequest(u string) {
 	fmt.Println("Performing GET request to " + u)
-
+	fmt.Println("----------------------------------------")
 	// Perform GET request here
-
 	response, err := http.Get(u)
 	checkError(err)
 	defer response.Body.Close()
 
 	fmt.Println("Response status:", response.Status)
-	// Get the body of the response
 	fmt.Println("Response size in KB:", response.ContentLength)
 
-	var responseString strings.Builder
-	body, err := io.ReadAll(response.Body)
+	fmt.Println("----------------------------------------")
+	// Read the response body
+	fmt.Println("Reading the response body")
+
+	var responseString strings.Builder     // Creating a variable of type strings.Builder to store the response body
+	body, err := io.ReadAll(response.Body) // Again we use io.ReadAll to read the response body
+	checkError(err)                        // fmt.Println("Response body:", string(body))
+
+	byteCount, err := responseString.Write(body)
 	checkError(err)
-	// fmt.Println("Response body:", string(body))
-	byteCount, err := responseString.Write(body) // Write the body to the string builder. We use Write because it returns the number of bytes written and we use responseString.String() to get the string representation of the builder. It is more efficient than using string concatenation or doing string(body) because it doesn't create a new string every time we append to it.
-	checkError(err)
+	// Write the body to the string builder. We use Write because it returns the number of bytes written
+	// and we use responseString.String() to get the string representation of the builder.
+	// It is more efficient than using string concatenation or doing string(body) because
+	// it doesn't create a new string every time we append to it.
+
 	fmt.Println("Byte count: ", byteCount)
 	fmt.Println("Response body:", responseString.String())
+	fmt.Println("========================================")
 
 }
 
+// ----------------- POST Request ----------------- //
+// PerformPostFormRequest performs a POST request to the specified URL with form data
+func PerformPostFormRequest(u string) {
+	fmt.Println("Performing POST request to " + u)
+	fmt.Println("----------------------------------------")
+
+	// Create form data using url.Values
+	data := url.Values{}      // Vlaues is a type that represents form data. It is a map[string][]string type.
+	data.Add("name", "Monal") // Add key-value pairs to the form data
+	data.Add("age", "22")
+	data.Add("job", "Web Developer")
+
+	// Perform POST request with form data
+	res, err := http.PostForm(u, data)
+	checkError(err)
+	defer res.Body.Close()
+
+	fmt.Println("Response status:", res.Status)
+	fmt.Println("----------------------------------------")
+
+	// Read the response body
+	fmt.Println("Reading the response body")
+	content, err := io.ReadAll(res.Body) // Read the response body using io.ReadAll
+	checkError(err)
+
+	// Print the response body
+	var responseString strings.Builder
+	responseString.Write(content) // This will write the content to the variable responseString of type strings.Builder
+	fmt.Println("Response body:", responseString.String())
+	fmt.Println("========================================")
+}
+
+// checkError checks if an error occurred and panics if it did
 func checkError(err error) {
 	if err != nil {
 		panic(err)
